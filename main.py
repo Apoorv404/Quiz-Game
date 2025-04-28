68368e4da70f3d7e45a8b63872c41d474e071611
Apoorv404 <jadhavapoorv2244@gmail.com>
2025-04-28 15:28:26 +0530

    added GUI

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from question_model import Question
-from data import  question_data
+from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
-for item in question_data:
-    question_text = item["text"]
-    question_answer = item["answer"]
-    new_question = Question(q_text=question_text,q_answer=question_answer)
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-
-while quiz.still_has_question():
-    quiz.next_question()
-
-print("You've completed the quiz.")
-print(f"Your final score: {quiz.score}/{quiz.question_number}")
+quiz_interface = QuizInterface(quiz)
